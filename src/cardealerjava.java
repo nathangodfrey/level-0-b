2c59f04b60ec2cd98bced4f24a0626f094dce018
Savera <Level 2>
2016-12-08 18:29:12 -0800

    cardealerjava

--- src/cardealerjava.java
+++ src/cardealerjava.java
@@ -6,8 +6,15 @@ String car = JOptionPane.showInputDialog("What kind of car do you want" );
 JOptionPane.showMessageDialog(null, car);
 if(car.equals("limo")){
 	JOptionPane.showMessageDialog(null, "your limo is 100,000,000");
-
+	
 }
+if(car.equals("honda")){
+JOptionPane.showMessageDialog(null, "your honda is 15,620");	
 }
 
+if(car.equals(toyota))
+}
+
+	
+}
 }

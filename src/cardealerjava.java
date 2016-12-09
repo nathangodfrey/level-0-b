24e5b702576cbfa51f474a9426f5e3f3b913adca
Savera <Level 2>
2016-12-08 18:27:58 -0800

    today I made a car dealer java

---
+++ src/cardealerjava.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class cardealerjava {
+public static void main(String[] args) {
+String car = JOptionPane.showInputDialog("What kind of car do you want" );
+JOptionPane.showMessageDialog(null, car);
+if(car.equals("limo")){
+	JOptionPane.showMessageDialog(null, "your limo is 100,000,000");
+
+}
+}
+
+}

f86cbac2df672bd73aabd9c2d98e5d0397f86eb7
nathangodfrey <league@Mac-mini-2.local>
2017-01-19 18:31:55 -0800

    Algebra solver

---
+++ src/AlgebraSolver.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class AlgebraSolver {
+public static void main(String[] args) {
+String monster=JOptionPane.showInputDialog(null, "Enter a Multiplier");	
+
+}
+}
